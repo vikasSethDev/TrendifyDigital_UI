f58e5a04ccab648007348076cbc9c004fe88126f
Seth, Vikas (BOM-MLL) <Vikas.Seth@mullenlowelintas.in>
2025-11-03 14:23:07 +0530

    envoirment change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,6 @@
 export const environment = {
   production: false,
   // This must match the server's PORT (5000) and API prefix (api/)
-  apiBaseUrl: 'http://localhost:5000/api/' 
+  // apiBaseUrl: 'http://localhost:5000/api/' 
+  apiBaseUrl: 'https://trendify-api-sesi.onrender.com/api/'
 };
